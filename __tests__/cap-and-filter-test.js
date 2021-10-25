34fa900b433cf075fd52041987a246fe33b9d9e3
diyanamendoza <diyana.amp@pm.me>
2021-10-25 16:53:54 -0700

    write test for capAndFilter

---
+++ __tests__/cap-and-filter-test.js
@@ -0,0 +1,9 @@
+const capAndFilter = require('../cap-and-filter.js');
+
+describe('capAndFilter', () => {
+  it('takes an array of strings, capitalizes all strings and filters out any string that starts with the letter F/f', () => {
+    const array = ['red', 'green', 'flower', 'Factor'];
+
+    expect(capAndFilter(array).toEqual(['RED', 'GREEN']));
+  });
+});
