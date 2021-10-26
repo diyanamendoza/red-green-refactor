81dea5fcc3f69b06e2d1d74e33e1647622ad46d2
diyanamendoza <diyana.amp@pm.me>
2021-10-26 11:06:49 -0700

    fetchQuotes test pass with jest.mock

--- __tests__/fetch-quotes-test.js
+++ __tests__/fetch-quotes-test.js
@@ -1,7 +1,20 @@
 const fetchQuotes = require('../fetch-quotes.js');
+const fetch = require('cross-fetch');
+
+jest.mock('cross-fetch');
 
 describe('fetchQuotes', () => {
   it('returns a single quote from the API', async () => {
+    fetch.mockResolvedValue({
+      json: async () => [
+        {
+          'character': 'Bender',
+          'quote': 'Computer dating: it\'s just like pimping except you rarely have to use the\nterm \'upside your head\'.',
+          'image': 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
+        }
+      ]
+    });
+
     const quote = await fetchQuotes();
     
     const expectation = {
