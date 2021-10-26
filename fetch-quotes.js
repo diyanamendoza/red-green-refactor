7e803c468e184778da04fe54b652afdc462f29cd
diyanamendoza <diyana.amp@pm.me>
2021-10-25 17:27:31 -0700

    fetchQuotes test pass

--- fetch-quotes.js
+++ fetch-quotes.js
@@ -1,7 +1,10 @@
 const fetch = require('cross-fetch');
 
 const fetchQuotes = async () => {
-
+  const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
+  const body = await res.json();
+  const { character, quote, image } = body[0];
+  return { 'name': character, 'text': quote, image };
 };
   
 module.exports = fetchQuotes;
